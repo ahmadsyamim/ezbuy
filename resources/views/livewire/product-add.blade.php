01b2e7f0a55d42abf8405f860ba245a0040a53bd
lokmanulhakim <ulhakim@yahoo.com>
2022-07-12 01:54:01 +0900

    button changes

--- resources/views/livewire/product-add.blade.php
+++ resources/views/livewire/product-add.blade.php
@@ -18,11 +18,11 @@
         <div  class="input-group" style="max-width:400px;">
             <div class="ui action input">
                 <input wire:model="search" class="form-control required" type="text" placeholder="Enter your product link"/>
-                <button class="ui red button btn-add-product" wire:loading.class="loading" wire:click="add">Get Total Price</button>
+                <button class="ui red button btn-add-product" wire:loading.class="loading" wire:click="add">Quote For Me</button>
             </div>
             {{-- <div class="input-group-text"><i class="icon-calculator1"></i></div>
             <input wire:model="search" class="form-control required" type="text" placeholder="Enter your product link"/>
-            <button class="btn btn-danger btn-add-product" wire:click="add">Get Total Price</button> --}}
+            <button class="btn btn-danger btn-add-product" wire:click="add">Quote For Me</button> --}}
         </div>
     @endif
     <script>
